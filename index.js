6aa895371d245cffd7680ffff0e089de7664b1d3
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2020-02-20 17:56:51 -0500

    cleaned up browser/page

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
-const logger = require("./logger");
+const logger = require("./logger")(module)
 const connect = require("./mongodb/connect");
 const pupeteer = require('puppeteer');
 const cheerio = require('cheerio');
-const { SASCSchema, SFRCSchema } = require("./mongodb/schemas");
+const { SFRCSchema } = require("./mongodb/schemas");
 const asyncForEach = async (array, callback) => {
   for (let index = 0; index < array.length; index++) {
     await callback(array[index]);
   }
 };
 
-let range = Array(49)
+let range = Array(66)
   .fill(1)
   .map((x, y) => x + y);
 
-const getHearings = async url => {
-  const browser = await pupeteer.launch({headless: true});
-  const page = await browser.newPage();
+const getHearings = async (url, page) => {
   await page.goto(url, {waitUntil: 'networkidle2'}); // Ensure no network requests are happening (in last 500ms).
   let html = await page.content();
   let cleaned = html.replace(/[\t\n]+/g,' ');
@@ -27,13 +25,16 @@ const getHearings = async url => {
   let $rows = $('tr.vevent')
     .map((i, v) => $(v).find('td'));
   $rows.each((i, v) => {
-    let link = $(v[0])
+    let linkBase = 'https://www.foreign.senate.gov';
+    let linkRef = $(v[0])
       .find('a')
       .attr('href');
+    let link = linkBase.concat(linkRef);
     let title = $(v[0])
       .find('a')
       .first()
       .text()
+      .replace(/\s\s+/g, " ")
       .trim();
     let location = $(v[1])
       .text()
@@ -47,17 +48,22 @@ const getHearings = async url => {
     res.push({link, title, location, time, date});
   });
 
-  await browser.close();
   return res;
 };
 
 let execute = async () => {
   connect(); 
+
+  const browser = await pupeteer.launch({headless: true});
+  const page = await browser.newPage();
+  
   await asyncForEach(range, async(pageNum) => {
-    let hearings = await getHearings(`https://www.armed-services.senate.gov/hearings?PageNum_rs=${pageNum}&c=all`);
-    await SASCSchema.insertMany(hearings);
+    let hearings = await getHearings(`https://www.foreign.senate.gov/hearings?PageNum_rs=${pageNum}`, page);
+    await SFRCSchema.insertMany(hearings);
     logger.info(`Added ${hearings.length} new records.`);
   });
+
+  await browser.close();
   logger.info('Done');
 };
 
